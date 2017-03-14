b1e20fced18005ce1c01a237a13abffbeef09f2b
Paul Thompson <mailto.prt@gmail.com>
2017-03-14 16:26:37 -0400

    logger.js

--- logger.js
+++ logger.js
@@ -1,7 +1,15 @@
 'use strict';
 
-// provide a fcn that accepts log events
+let fs = require('fs');
+let log_path = './logs/';
+let log_name = 'log'; // will extend name with each log file written
+let moment = require('moment');
 
+// provide a fcn that accepts log events
+function log(){
+
+
+}
 // write log events with details to a file
 
 // TODO: a log reader?
